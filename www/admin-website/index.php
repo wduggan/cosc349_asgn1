53b97ee6e8b243fec46d85c64224e00579c8f7db
William Duggan <williamduggan953@gmail.com>
2021-09-02 23:20:45 +1200

    Created directories for the web page files, which match the config files. Added test home pages

---
+++ www/admin-website/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>index</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+
+    <body>
+        <h1>Welcome to Admin Page</h1>
+        <p>Testing</p>
+    </body>
+
+</html>

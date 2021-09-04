0d6cbbc95907617f8a62ddcc57b599d2155f6bd4
William Duggan <williamduggan953@gmail.com>
2021-09-04 17:26:46 +1200

    changed index page for customer website, and added a stylesheet

--- www/customer-website/index.php
+++ www/customer-website/index.php
@@ -1,14 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>index</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
 
-    <body>
-        <h1>Welcome to Customer Page</h1>
-        <p>Testing</p>
-    </body>
+<head>
+    <title>Customer Website</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <section class="top-box">
+        <a href="">Admin</a>
+    </section>
+
+    <header>
+        <nav>
+            <ul>
+                <li><a>Home</a></li>
+                <li><a href="viewProducts.php">View Products</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+    <main>
+        <section class="boxes">
+            <h2>Welcome to the Customer Website</h2>
+            <p>...</p>
+        </section>
+    </main>
+
+    <footer class="boxes">
+        <p>We sell stuff</p>
+    </footer>
+
+</body>
 
 </html>

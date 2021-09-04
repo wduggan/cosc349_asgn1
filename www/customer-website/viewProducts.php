cfe8d351ce2c48183984554b892d5b8d4c816a63
William Duggan <williamduggan953@gmail.com>
2021-09-04 17:37:17 +1200

    Created page for products to be viewed on by the customer

---
+++ www/customer-website/viewProducts.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Customer Website</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+	<section class="top-box">
+		<a href="">Admin</a>
+	</section>
+
+	<header>
+		<nav>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a>View Products</a></li>
+			</ul>
+		</nav>
+	</header>
+
+
+	<main>
+		<section class="boxes">
+
+			<h2>View Products:</h2>
+
+			<table>
+				<thead>
+					<tr>
+						<th>Product-ID</th>
+						<th>Name</th>
+						<th>Description</th>
+						<th>Price</th>
+						<th>Quantity</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>ID</td>
+						<td>Name</td>
+						<td>Description</td>
+						<td>Price</td>
+						<td>Quantity</td>
+						<td><button>Buy</button></td>
+					</tr>
+				</tbody>
+			</table>
+
+		</section>
+	</main>
+
+	<footer class="boxes">
+		<p>We sell stuff</p>
+	</footer>
+
+</body>
+
+</html>
